043353465b1730552c44c30b5be5c5c25e55a195
emanuelB1 <emanuel.bus.or@gmail.com>
2024-02-29 22:45:11 -0300

    ajustes del fondo

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,15 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
+
+
+document.body.addEventListener("pointermove", (e)=>{
+  const { currentTarget: el, clientX: x, clientY: y } = e;
+  const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+  el.style.setProperty('--posX',  x-l-w/2);
+  el.style.setProperty('--posY',  y-t-h/2);
+})
+
+
+
+
